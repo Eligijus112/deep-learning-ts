2b667f67f9ce3d35a3e3c7ec6c391fae304dfd8d
Eligijus Bujokas <elasorama@gmail.com>
2020-02-09 13:31:11 +0200

    Added the pipeline configurations file conf.yml

--- deep_model.py
+++ deep_model.py
@@ -18,7 +18,8 @@ class DeepModelTS():
         lag: int,
         LSTM_layer_depth: int, 
         epochs=10, 
-        batch_size=256
+        batch_size=256,
+        train_test_split=0
     ):
 
         self.data = data 
@@ -27,6 +28,29 @@ class DeepModelTS():
         self.LSTM_layer_depth = LSTM_layer_depth
         self.batch_size = batch_size
         self.epochs = epochs
+        self.train_test_split = train_test_split
+
+    @staticmethod
+    def create_X_Y(ts: list, lag: int):
+        """
+        A method to create X and Y matrix from a time series list for the training of 
+        deep learning models 
+        """
+        X, Y = [], []
+
+        if len(ts) - lag <= 0:
+            X.append(ts)
+        else:
+            for i in range(len(ts) - lag):
+                Y.append(ts[i + lag])
+                X.append(ts[i:(i + lag)])
+
+        X, Y = np.array(X), np.array(Y)
+
+        # Reshaping the X array to an LSTM input shape 
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+        return X, Y         
 
     def create_data_for_NN(
         self,
@@ -43,29 +67,31 @@ class DeepModelTS():
             y = y[-use_last_n:]
 
         # The X matrix will hold the lags of Y 
-        X, Y = [], []
+        X, Y = self.create_X_Y(y, self.lag)
 
-        if len(y) - self.lag <= 0:
-            X.append(y)
-        else:
-            for i in range(len(y) - self.lag):
-                Y.append(y[i + self.lag])
-                X.append(y[i:(i + self.lag)])
-        
-        X = np.array(X)
-        Y = np.array(Y)
+        # Creating training and test sets 
+        X_train = X
+        X_test = []
 
-        # Reshaping the X array to an LSTM input shape 
-        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+        Y_train = Y
+        Y_test = []
 
-        return X, Y
+        if self.train_test_split > 0:
+            index = round(len(X) * self.train_test_split)
+            X_train = X[:(len(X) - index)]
+            X_test = X[-index:]     
+            
+            Y_train = Y[:(len(X) - index)]
+            Y_test = Y[-index:]
+
+        return X_train, X_test, Y_train, Y_test
 
     def LSTModel(self):
         """
         A method to fit the LSTM model 
         """
         # Getting the data 
-        X, Y = self.create_data_for_NN()
+        X_train, X_test, Y_train, Y_test = self.create_data_for_NN()
 
         # Defining the model
         model = Sequential()
@@ -73,41 +99,42 @@ class DeepModelTS():
         model.add(Dense(1))
         model.compile(optimizer='adam', loss='mse')
 
+        # Defining the model parameter dict 
+        keras_dict = {
+            'x': X_train,
+            'y': Y_train,
+            'batch_size': self.batch_size,
+            'epochs': self.epochs,
+            'shuffle': False
+        }
+
+        if self.train_test_split > 0:
+            keras_dict.update({
+                'validation_data': (X_test, Y_test)
+            })
+
         # Fitting the model 
         model.fit(
-            x=X,
-            y=Y, 
-            batch_size=self.batch_size,
-            epochs=self.epochs
+            **keras_dict
         )
+
+        # Saving the model to the class 
+        self.model = model
 
         return model
 
-    def predict(self, model, n_ahead):
+    def predict(self):
         """
-        A method to predict n steps ahead using the data which was used in 
-        creating the DeepModelTS class
+        A method to predict using the test data used in creating the class
         """
-        # Getting the last n time series 
-        X, _ = self.create_data_for_NN(use_last_n=self.lag)        
-
-        # Making the prediction list 
         yhat = []
 
-        for _ in range(n_ahead):
-            # Making the prediction
-            fc = model.predict(X)
-            yhat.append(fc)
-            
-            # Creating a new input matrix for forecasting
-            X = np.append(X, fc)
+        if(self.train_test_split > 0):
+        
+            # Getting the last n time series 
+            _, X_test, _, _ = self.create_data_for_NN()        
 
-            # Ommiting the first variable
-            X = np.delete(X, 0)
-
-            # Reshaping for the next iteration
-            X = np.reshape(X, (1, len(X), 1))
-
-        yhat = [y[0][0] for y in yhat]
+            # Making the prediction list 
+            yhat = [y[0] for y in self.model.predict(X_test)]
 
         return yhat
--- pipeline.py
+++ pipeline.py
@@ -1,22 +1,66 @@
+# Ploting packages
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Date wrangling
+from datetime import datetime, timedelta
+
 # Data wrangling
 import pandas as pd 
 
 # The deep learning class
 from deep_model import DeepModelTS
 
+# Reading the configuration file
+import yaml
+
+# Directory managment 
+import os
+
+# Reading the hyper parameters for the pipeline
+with open(f'{os.getcwd()}\\conf.yml') as file:
+    conf = yaml.load(file, Loader=yaml.FullLoader)
+
 # Reading the data 
 d = pd.read_csv('input/DAYTON_hourly.csv')
+d['Datetime'] = [datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in d['Datetime']]
+d.sort_values('Datetime', inplace=True)
 
 # Initiating the class 
 deep_learner = DeepModelTS(
     data=d, 
     Y_var='DAYTON_MW',
-    lag=24,
-    LSTM_layer_depth=100
+    lag=conf.get('lag'),
+    LSTM_layer_depth=conf.get('LSTM_layer_depth'),
+    epochs=conf.get('epochs'),
+    train_test_split=conf.get('train_test_split') # The share of data that will be used for validation
 )
 
 # Fitting the model 
 model = deep_learner.LSTModel()
 
-# Making the prediction for the next 48 hours
-yhat = deep_learner.predict(model, 48)
+# Making the prediction on the validation set
+# Only applicable if train_test_split in the conf.yml > 0
+yhat = deep_learner.predict()
+
+if len(yhat) > 0:
+
+    # Constructing the forecast dataframe
+    fc = d.tail(len(yhat)).copy()
+    fc.reset_index(inplace=True)
+    fc['forecast'] = yhat
+
+    # Ploting the forecasts
+    plt.figure(figsize=(12, 8))
+    for dtype in ['DAYTON_MW', 'forecast']:
+        plt.plot(
+            'Datetime',
+            dtype,
+            data=fc,
+            label=dtype,
+            alpha=0.8
+        )
+    plt.legend()
+    plt.grid()
+    plt.show()   
